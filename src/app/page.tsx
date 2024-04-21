2018526199cf4930f98f0900dedeffa5200833dd
aryandharwa <bb1221390@iitd.ac.in>
2024-04-22 02:06:16 +0530

    spotlight and button added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
-    <h1>Hello, World!</h1>
+    <main>
+    <HeroSection />
+    </main>
   );
 }
