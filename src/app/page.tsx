03ad6b8746b155f00adf04154d3f608462caa210
aryandharwa <bb1221390@iitd.ac.in>
2024-05-10 00:31:56 +0530

    sticky scroll section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main>
     <HeroSection />
     <FeaturedCourses />
+    <WhyChooseUs />
     </main>
   );
 }
